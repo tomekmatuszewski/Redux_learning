cdae42aab0021767a3310746f85e9cc859dbfeb6
TomekM <tomasz_matuszewski@o2.pl>
2020-11-10 21:24:01 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const initialMovies = {
+  listName: "Favourite",
+  list: [
+    'Movie 1', 'Movie 2', 'Movie3'
+  ]
+}
+
+const initialActors = {
+  listName: 'Best',
+  list: [
+    'Tom Hanks', 'Julia Roberts', 'Brad Pitt'
+  ]
+}
+
+
+function movies(state = initialMovies, action) {
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return{
+        ...state, list: [...state.list, action.item]
+      }
+    case 'RESET_MOVIES':
+      return {
+        ...state, list: []
+      }
+    default:
+      return state
+  }
+}
+
+function actors(state = initialActors, action) {
+  switch (action.type) {
+    case 'ADD_ACTOR':
+      return{
+        ...state, list: [...state.list, action.item]
+      }
+    case 'RESET_ACTORS':
+      return {
+        ...state, list: []
+      }
+    default:
+      return state
+  }
+}
+
+
+
+const allReducers = combineReducers({movies, actors})
+
+
+const store = createStore(allReducers, composeWithDevTools())
+window.store = store
+
+
+
+
 function App() {
   return (
     <div className="App">
