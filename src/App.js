7e47792ad84fdb6be5f80b292861b7610fc26aea
TomekM <tomasz_matuszewski@o2.pl>
2020-11-12 22:21:04 +0100

    dispatch from react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import MoviesContainer from './app/movies/components/MoviesContainer'
 
+import MoviesContainer from './app/movies/components/MoviesContainer'
+import MoviesForm from './app/movies/components/MoviesForm'
 
 function App() {
   return (
     <div className="App">
       
         <MoviesContainer />
+        <MoviesForm/>
   
     </div>
   );
